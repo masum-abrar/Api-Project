9a654877dae710f2908455714c453a49784b4dce
masum-abrar <masum.abrar02@gmail.com>
2026-05-15 03:33:13 +0600

    My first commit

---
+++ report.js
@@ -0,0 +1,27 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+   
+    collection:require('./Dmoney_Api_Assignment.postman_collection.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+     delayRequest: 5000, 
+         envVar: [
+        {
+
+            
+            key: "gmailToken",
+            value:  process.env.token
+        }
+    ],
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html',
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
+
